b83f02277ee7d81af7f17aeb02e566f18bb0da88
Parikshit <parikshitpandey03@gmail.com>
2025-07-29 00:29:35 +0530

    new changes

--- app.js
+++ app.js
@@ -1,124 +1,95 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js")
-const path = require("path")
-const methodOverride = require("method-override")
+const path = require("path");
+const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-// data Base ke liye ham ek async function banaenge 
-const MONGO_URL = "mongodb://127.0.0.1:27017/Yatra"
+const listingsRouter = require("./routes/listing.js");
+const reviewsRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
-main().then(()=>{
+// data Base ke liye ham ek async function banaenge
+const MONGO_URL = "mongodb://127.0.0.1:27017/Yatra";
+
+main()
+  .then(() => {
     console.log("Connected to DB");
-}).catch(err => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
-async function main(){
-    await mongoose.connect(MONGO_URL); 
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended: true}));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs',ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.get('/',(req,res) => {
-    res.send("Hello");
+const sessionOptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000, // time in ms
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello");
 });
 
-// index route
-app.get('/listings', wrapAsync(async (req,res) => {
-    const allListings = await Listing.find({});
-    res.render("./listings/index.ejs",{allListings});
-}));
+app.use(session(sessionOptions));
+app.use(flash());
 
-//new route
-app.get('/listings/new', wrapAsync(async (req,res) => {
-    res.render("./listings/new.ejs");
-}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-// show route
-app.get('/listings/:id', wrapAsync(async (req,res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("./listings/show.ejs",{listing});
-}));
+app.use((req,res,next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
-// add route
-
-app.post('/listings/', wrapAsync(async (req, res, next) => {
-    if(!req.body.listing){
-        throw new ExpressError(400,"Send Valid data for listing");
-    }
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect('/listings');
-}));
-
-
-
-//Edit Route
-app.get('/listings/:id/edit/', wrapAsync(async (req,res) => {
-     let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("./listings/edit.ejs",{listing});
-}));
-
-// Update Route
-app.put('/listings/:id', wrapAsync(async (req,res) => {
-    let{id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}));
-
-// DELETE ROUTE
-app.delete('/listings/:id', wrapAsync(async (req,res) => {
-    let {id} = req.params;
-    await Listing.findByIdAndDelete(id);
-    res.redirect("/listings");
-}));
-
-
-// TEst rouTe
-// app.get('/debug', (req, res) => {
-//     res.send('Debug route is working');
+// app.get("/demouser", async (req,res) => {
+//   let fakeUser = new User({
+//     email: "student@gmail.com",
+//     username: "Learner",
+//   });
+//   let registeredUser = await User.register(fakeUser, "password");
+//   res.send(registeredUser);
 // });
 
-// app.get('/cause-error', (req, res) => {
-//     res.redirect('/:');  // Will intentionally crash with the same error
-// });
-
-
-// app.get('/test',async (req,res) => {
-//     let sampleListing = new Listing({
-//         title: "travel",
-//         description: "abcd",
-//         price: 1100,
-//         location: "Goa",
-//         country: "India",
-//     });
-
-//     await sampleListing.save();
-//     console.log("Sample was saved");
-//     res.send("Sucessful Testing");
-// });
+app.use("/listings", listingsRouter);
+app.use("/listings/:id/reviews", reviewsRouter);
+app.use("/", userRouter);
 
 // app.all("*" ,(req, res, next) => {
 //      next(new ExpressError(404,"Page Not Found!"));
 // });
 
-app.use((err,req,res,next) => {
-    let{statusCode=500, message="Something Went Wrong!"} = err;
-    res.status(statusCode).send(message);
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something Went Wrong!" } = err;
+  res.status(statusCode).render("error.ejs", { message });
 });
 
 app.listen(8080, () => {
-    console.log("Server is listening at port 8080");
+  console.log("Server is listening at port 8080");
 });
